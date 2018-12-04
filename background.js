cc5d714318f40ca9f87b4017a24989c2a5b68799
xyalbino <xyalbino@163.com>
2018-12-03 20:15:19 -0500

    CORS request

--- background.js
+++ background.js
@@ -1,8 +1,26 @@
 dict = [];
+const HEADER_CORS = {
+    "accept": "accept",
+    "accept-language": "accept-language",
+    "content-language": "content-language",
+    "content-type": "content-type",
+    "drp": "drp",
+    "downlink": "downlink",
+    "save-data": "save-data",
+    "viewport-width": "viweport-width",
+    "width": "width"
+}
+
 chrome.webRequest.onBeforeSendHeaders.addListener(
     function(details) {	
-    	if (details.type == "websocket")
+        var headers=returnHeaders(details);
+        if (headers.hasOwnProperty('Upgrade') && headers['Upgrade']=='websocket')
+    	//if (headers.type == "websocket")
     		handleWebSocket(details)
+        else if (checkSimpleCORS(details))  {
+            console.log(details.type);
+            handleSimpleCORS(headers);
+        }
         dict.push({ key: details.tabId, value: details.requestHeaders });
         //console.log(dict)
       return {requestHeaders: details.requestHeaders};
@@ -21,6 +39,21 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     
 });
 
+//send the wrong message
+function sendWrong(errors){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  		chrome.tabs.sendMessage(tabs[0].id, {alert: "true",errors:errors})
+		})
+}
+
+function returnHeaders(datails){
+    const type = details.hasOwnProperty('requestHeaders') ? 'request' : 'response';
+    var _headers = details[`${type}Headers`];
+    var headers={};
+    for (let header of _headers) { headers[header.name]= header.value;}
+    return(headers);
+}
+
 function handleWebSocket(details){
 
 	if (details.initiator.indexOf("http://") != -1 || details.initiator.indexOf("https://") != -1)
@@ -35,12 +68,53 @@ function handleWebSocket(details){
 	initiatorDomain = getDomainName(initiatorDomain)
 	urlDomain = getDomainName(urlDomain)
 
-	if (initiatorDomain != urlDomain){
+	if (initiatorDomain != urlDomain)  sendWrong("ws_hijacking");
+}
 
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	  		chrome.tabs.sendMessage(tabs[0].id, {alert: "true"})
-		})
-	}
+function titleForSimpleCORS(name) {
+    name = name.toLowerCase();
+    if (HEADER_CORS.hasOwnProperty(name)){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//Check whether the CORS request is a simple one
+function checkSimpleCORS(details){
+    if (details.method != "HEAD" && details.methond != "GET" && details.method != "POST") {
+        return false;
+    }    // Request methond is HEAD, GET or POST
+    
+    const type = details.hasOwnProperty('requestHeaders') ? 'request' : 'response';
+    var _headers = details[`${type}Headers`];
+    var headers={};
+    for (let header of _headers) { headers[header.name]= header.value;}
+    var headerNames = Object.keys(headers).sort();
+    for (let name of headerNames){
+        if (!titleForSimpleCORS(name)) {
+            return false;
+            console.log(name);
+            }
+        } //Request header values are not customized, except for 9 whitelisted headers in HEADER_CORS
+    
+    if(headers.hasOwnProperty('Content-Type')){
+        if (headers['Content-Type']!= "text/plain" && headers['Content-Type'] != "multipart/form-data" && headers['Content-Type'] != "application/x-form-uri-encoded"){
+        return false;
+        } //Content-Type header value is one of three specific values
+    }
+    
+    return true;
+}
+
+function handleSimpleCORS(headers){
+    var regex=/[{}()]/g;
+    for (let header of ['Accept','Accept-Language','Content-Language']){
+        if (headers.hasOwnProperty(header)){
+            if(headers[header].match(regex) != null) sendWrong('CORSattack');
+        } //restrict the values of Accept, Accept-Language and Content-Language
+    }
 }
 
 function extractDomainHttp(url){
@@ -53,8 +127,9 @@ function extractDomainWss(url){
 
 function getDomainName(url){
 
-	if ((url.match(/\./g)||[]).length == 1)
+	if ((url.match(/\./g)||[]).length == 1){
 		return url.substring(url.indexOf("://")+3)
+    }
 	else{
 
 		arr = url.split('.')
